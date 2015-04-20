darklordIN/platform_packages_apps_Settings

Settings: notification drawer background (1/2)
    so after keyguard is mostly done let us start notification drawer
    and QS. First one custom background.....either color or picture
    with custom alpha and ability to set a landscape picture seperatly
     if the user want. (lanscape phone only)

    Compared to the 4.3 included version completly recoded and better
    integrated. No idea what I thought 1 year ago on the old implementation

    cheeers

Change-Id: I1c8a1f1256d96c6ea5c1b160814b9aa6835318f3

src/com/android/settings/crdroid/SeekBarPreference.java
@@ -1,0 +1,123 @@
+package com.android.settings.crdroid;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.TypedArray;
+import android.preference.Preference;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+import com.android.settings.R;
+
+import android.provider.Settings;
+
+public class SeekBarPreference extends Preference
+        implements OnSeekBarChangeListener {
+
+    public static int maximum = 100;
+    public static int interval = 5;
+
+    private String property;
+
+    private TextView monitorBox;
+    private SeekBar bar;
+
+    int defaultValue = 60;
+    boolean mDisablePercentageValue = false;
+
+    private OnPreferenceChangeListener changer;
+
+    public SeekBarPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected View onCreateView(ViewGroup parent) {
+
+        View layout = View.inflate(getContext(), R.layout.slider_preference, null);
+
+        monitorBox = (TextView) layout.findViewById(R.id.monitor_box);
+        bar = (SeekBar) layout.findViewById(R.id.seek_bar);
+        int progress;
+        try{
+            progress = (int) (Settings.System.getFloat(
+                    getContext().getContentResolver(), property) * 100);
+        } catch (Exception e) {
+            progress = defaultValue;
+        }
+        bar.setOnSeekBarChangeListener(this);
+        bar.setProgress(progress);
+        if (!mDisablePercentageValue) {
+            monitorBox.setText(progress + "%");
+        }
+        return layout;
+    }
+
+    public void setInitValue(int progress) {
+        defaultValue = progress;
+        if (bar!=null) {
+            bar.setProgress(progress);
+            if (!mDisablePercentageValue) {
+                monitorBox.setText(progress + "%");
+            }
+        }
+    }
+
+    @Override
+    protected Object onGetDefaultValue(TypedArray a, int index) {
+        // TODO Auto-generated method stub
+        return super.onGetDefaultValue(a, index);
+    }
+
+    @Override
+    public void setOnPreferenceChangeListener(
+                OnPreferenceChangeListener onPreferenceChangeListener) {
+        changer = onPreferenceChangeListener;
+        super.setOnPreferenceChangeListener(onPreferenceChangeListener);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+        progress = Math.round(((float) progress) / interval) * interval;
+        seekBar.setProgress(progress);
+
+        if (!mDisablePercentageValue) {
+            monitorBox.setText(progress + "%");
+        }
+        changer.onPreferenceChange(this, Integer.toString(progress));
+    }
+
+    public void setValue(int progress){
+        if (bar!=null) {
+            bar.setProgress(progress);
+            if (!mDisablePercentageValue) {
+                monitorBox.setText(progress + "%");
+            }
+            changer.onPreferenceChange(this, Integer.toString(progress));
+        }
+    }
+
+    public void disablePercentageValue(boolean disable) {
+        mDisablePercentageValue = disable;
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+}

ABIP/android_packages_apps_Settings

src/com/android/settings/batik/SeekBarPreference.java
@@ -1,0 +1,123 @@
+package com.android.settings.batik;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.TypedArray;
+import android.preference.Preference;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+import com.android.settings.R;
+
+import android.provider.Settings;
+
+public class SeekBarPreference extends Preference
+        implements OnSeekBarChangeListener {
+
+    public static int maximum = 100;
+    public static int interval = 5;
+
+    private String property;
+
+    private TextView monitorBox;
+    private SeekBar bar;
+
+    int defaultValue = 60;
+    boolean mDisablePercentageValue = false;
+
+    private OnPreferenceChangeListener changer;
+
+    public SeekBarPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected View onCreateView(ViewGroup parent) {
+
+        View layout = View.inflate(getContext(), R.layout.slider_preference, null);
+
+        monitorBox = (TextView) layout.findViewById(R.id.monitor_box);
+        bar = (SeekBar) layout.findViewById(R.id.seek_bar);
+        int progress;
+        try{
+            progress = (int) (Settings.System.getFloat(
+                    getContext().getContentResolver(), property) * 100);
+        } catch (Exception e) {
+            progress = defaultValue;
+        }
+        bar.setOnSeekBarChangeListener(this);
+        bar.setProgress(progress);
+        if (!mDisablePercentageValue) {
+            monitorBox.setText(progress + "%");
+        }
+        return layout;
+    }
+
+    public void setInitValue(int progress) {
+        defaultValue = progress;
+        if (bar!=null) {
+            bar.setProgress(progress);
+            if (!mDisablePercentageValue) {
+                monitorBox.setText(progress + "%");
+            }
+        }
+    }
+
+    @Override
+    protected Object onGetDefaultValue(TypedArray a, int index) {
+        // TODO Auto-generated method stub
+        return super.onGetDefaultValue(a, index);
+    }
+
+    @Override
+    public void setOnPreferenceChangeListener(
+                OnPreferenceChangeListener onPreferenceChangeListener) {
+        changer = onPreferenceChangeListener;
+        super.setOnPreferenceChangeListener(onPreferenceChangeListener);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+        progress = Math.round(((float) progress) / interval) * interval;
+        seekBar.setProgress(progress);
+
+        if (!mDisablePercentageValue) {
+            monitorBox.setText(progress + "%");
+        }
+        changer.onPreferenceChange(this, Integer.toString(progress));
+    }
+
+    public void setValue(int progress){
+        if (bar!=null) {
+            bar.setProgress(progress);
+            if (!mDisablePercentageValue) {
+                monitorBox.setText(progress + "%");
+            }
+            changer.onPreferenceChange(this, Integer.toString(progress));
+        }
+    }
+
+    public void disablePercentageValue(boolean disable) {
+        mDisablePercentageValue = disable;
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+}